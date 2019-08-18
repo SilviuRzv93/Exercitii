821c8a3aba4f89f5f65ad40ba0dd653a61c1338b
SilviuRzv93 <[silviu.rzv93@gmail.com]>
2019-08-18 22:25:06 +0300

    first commit

--- src/SquareMatrix.java
+++ src/SquareMatrix.java
@@ -1,11 +1,11 @@
-public class SquareMatrix extends Matrix {
+public class SquareMatrix<T> extends Matrix<T> {
 
 
 
 
 public SquareMatrix(int size){
 
-    super(size , size);
+    super(T[][])new Object[1][2];
 }
 
 
