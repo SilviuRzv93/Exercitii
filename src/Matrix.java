15a9214258c22ee8dcf4b26031c3757ecaaaadf2
SilviuRzv93 <[silviu.rzv93@gmail.com]>
2019-08-18 23:35:01 +0300

    first commit

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,6 +1,6 @@
 public class Matrix<T> {
 
-    private T [][] arr ;
+    protected T [][] arr ;
     public Matrix(int row ,int cols){
 
         arr = (T[][])new Object[1][2];
--- src/SquareMatrix.java
+++ src/SquareMatrix.java
@@ -3,12 +3,22 @@ public class SquareMatrix<T> extends Matrix<T> {
 
 
 
-public SquareMatrix(int size){
+    public SquareMatrix(int size){
 
     super(size , size);
+
+
 }
 
+public T[] getPrimaryDiagonal(int size){
 
+        for(int i =0; i<arr.length;i++){
+
+            result[i]= arr[i][i];
+        }
+
+
+}
 
 
 
